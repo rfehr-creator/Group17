5988fb1310588aea15aece32eceabe3751f94350
Jose Gonzales <118575003+JoseGonzaless@users.noreply.github.com>
2022-11-21 18:07:07 -0600

    Update game.js

--- game.js
+++ game.js
@@ -142,6 +142,203 @@ function defaultGames() {
             "disliked": false,
             "inCart": false,
             "tags": ["FPS", "Action", "Shooter", "Multiplayer", "Military"],
+        },
+        {
+            "id": 3,
+            "name": "Counter-Strike: Global Offensive",
+            "description": "Counter-Strike: Global Offensive (CS: GO) expands upon the team-based action gameplay that it pioneered when it was launched 19 years ago. CS: GO features new maps, characters, weapons, and game modes, and delivers updated versions of the classic CS content (de_dust2, etc.).",
+            "price": "Free To Play",
+            "displayPicture": "https://cdn.cloudflare.steamstatic.com/steam/apps/730/header.jpg?t=1668125812",
+            "picutres":[
+                "https://cdn.cloudflare.steamstatic.com/steam/apps/81958/movie480.webm?t=1554409259" //trailer
+            ],
+            "liked": false,
+            "disliked": false,
+            "inCart": false,
+            "tags": ["FPS", "Shooter", "Multiplayer", "Competitive", "Action"],
+        },
+        {
+            "id": 4,
+            "name": "Rust",
+            "description": "The only aim in Rust is to survive. Everything wants you to die - the island’s wildlife and other inhabitants, the environment, other survivors. Do whatever it takes to last another night.",
+            "price": 50.49,
+            "displayPicture": "https://cdn.cloudflare.steamstatic.com/steam/apps/252490/header.jpg?t=1666865208",
+            "picutres":[
+                "https://cdn.cloudflare.steamstatic.com/steam/apps/256684736/movie480_vp9.webm?t=1624520315" //trailer
+            
+            ],
+            "liked": false,
+            "disliked": false,
+            "inCart": false,
+            "tags": ["Survival", "Crafting", "Multiplayer", "Open World"],
+        },
+        {
+            "id": 5,
+            "name": "Borderlands 3",
+            "description": "The original shooter-looter returns, packing bazillions of guns and a mayhem-fueled adventure! Blast through new worlds and enemies as one of four new Vault Hunters.",
+            "price": 79.99,
+            "displayPicture": "https://cdn.cloudflare.steamstatic.com/steam/apps/397540/header.jpg?t=1657214217",
+            "picutres":[
+                "https://cdn.cloudflare.steamstatic.com/steam/apps/256871049/movie480_vp9.webm?t=1643331503" //trailer
+            ],
+            "liked": false,
+            "disliked": false,
+            "inCart": false,
+            "tags": ["RPG", "Action", "Looter Shooter", "FPS", "Multiplayer"],
+        },
+        {
+            "id": 6,
+            "name": "Left 4 Dead 2",
+            "description": "Set in the zombie apocalypse, Left 4 Dead 2 (L4D2) is the highly anticipated sequel to the award-winning Left 4 Dead, the #1 co-op game of 2008. This co-operative action horror FPS takes you and your friends through the cities, swamps and cemeteries of the Deep South, from Savannah to New Orleans across five expansive campaigns.",
+            "price": 12.99,
+            "displayPicture": "https://cdn.cloudflare.steamstatic.com/steam/apps/550/header.jpg?t=1666824129",
+            "picutres":[
+                "https://cdn.cloudflare.steamstatic.com/steam/apps/5952/movie480.webm?t=1447353587" //trailer
+            ],
+            "liked": false,
+            "disliked": false,
+            "inCart": false,
+            "tags": ["Zombies", "Co-op", "FPS", "Multiplayer", "Shooter"],
+        },
+        {
+            "id": 7,
+            "name": "Stardew Valley",
+            "description": "You've inherited your grandfather's old farm plot in Stardew Valley. Armed with hand-me-down tools and a few coins, you set out to begin your new life. Can you learn to live off the land and turn these overgrown fields into a thriving home?",
+            "price": 16.99,
+            "displayPicture": "https://cdn.cloudflare.steamstatic.com/steam/apps/413150/header.jpg?t=1666917466",
+            "picutres":[
+                "https://cdn.cloudflare.steamstatic.com/steam/apps/256660296/movie480.webm?t=1454099186" //trailer
+            ],
+            "liked": false,
+            "disliked": false,
+            "inCart": false,
+            "tags": ["Farming Sim", "Life Sim", "Pixel Graphics", "RPG"],
+        },
+        {
+            "id": 8,
+            "name": "New World",
+            "description": "Explore a thrilling, open-world MMO filled with danger and opportunity where you'll forge a new destiny on the supernatural island of Aeternum.",
+            "price": 49.99,
+            "displayPicture": "https://cdn.cloudflare.steamstatic.com/steam/apps/1063730/header.jpg?t=1666108453",
+            "picutres":[
+                "https://cdn.cloudflare.steamstatic.com/steam/apps/256911386/movie480_vp9.webm?t=1666108429" //trailer
+            ],
+            "liked": false,
+            "disliked": false,
+            "inCart": false,
+            "tags": ["Massively Multiplayer", "Open World", "MMORPG"],
+        },
+        {
+            "id": 9,
+            "name": "FINAL FANTASY XIV Online",
+            "description": "Take part in an epic and ever-changing FINAL FANTASY as you adventure and explore with friends from around the world.",
+            "price": 24.99,
+            "displayPicture": "https://cdn.cloudflare.steamstatic.com/steam/apps/39210/header.jpg?t=1646695302",
+            "picutres":[
+                "https://cdn.cloudflare.steamstatic.com/steam/apps/2029865/movie480.webm?t=1447359267" //trailer
+            ],
+            "liked": false,
+            "disliked": false,
+            "inCart": false,
+            "tags": ["MMORPG", "RPG", "Massively Multiplayer", "Fantasy"],
+        },
+        {
+            "id": 10,
+            "name": "Lost Ark",
+            "description": "Embark on an odyssey for the Lost Ark in a vast, vibrant world: explore new lands, seek out lost treasures, and test yourself in thrilling action combat in this action-packed free-to-play RPG.",
+            "price": "Free To Play",
+            "displayPicture": "https://cdn.cloudflare.steamstatic.com/steam/apps/1599340/header.jpg?t=1668553287",
+            "picutres":[
+                "https://cdn.cloudflare.steamstatic.com/steam/apps/256916251/movie480_vp9.webm?t=1668553272" // trailer
+            ],
+            "liked": false,
+            "disliked": false,
+            "inCart": false,
+            "tags": ["MMORPG", "Free To Play", "Action RPG", "Multiplayer"],
+        },
+        {
+            "id": 11,
+            "name": "God of War",
+            "description": "His vengeance against the Gods of Olympus years behind him, Kratos now lives as a man in the realm of Norse Gods and monsters. It is in this harsh, unforgiving world that he must fight to survive… and teach his son to do the same.",
+            "price": 59.99,
+            "displayPicture": "https://cdn.cloudflare.steamstatic.com/steam/apps/1593500/header.jpg?t=1650554420",
+            "picutres":[
+                "https://cdn.cloudflare.steamstatic.com/steam/apps/256864004/movie480_vp9.webm?t=1639001817" //trailer
+            ],
+            "liked": false,
+            "disliked": false,
+            "inCart": false,
+            "tags": ["Action", "Singleplayer", "Adventure", "Story Rich", "3D"],
+        },
+        {
+            "id": 12,
+            "name": "Cyberpunk 2077",
+            "description": "Cyberpunk 2077 is an open-world, action-adventure RPG set in the dark future of Night City — a dangerous megalopolis obsessed with power, glamor, and ceaseless body modification.",
+            "price": 79.99,
+            "displayPicture": "https://cdn.cloudflare.steamstatic.com/steam/apps/1091500/header.jpg?t=1663663573",
+            "picutres":[
+                "https://cdn.cloudflare.steamstatic.com/steam/apps/256904407/movie480_vp9.webm?t=1662480414" //trailer
+            ],
+            "liked": false,
+            "disliked": false,
+            "inCart": false,
+            "tags": ["Cyberpunk", "Open World", "RPG", "Nudity", "Sci-fi"],
+        },
+        {
+            "id": 13,
+            "name": "The Witcher® 3: Wild Hunt",
+            "description": "As war rages on throughout the Northern Realms, you take on the greatest contract of your life — tracking down the Child of Prophecy, a living weapon that can alter the shape of the world.",
+            "price": 55.99,
+            "displayPicture": "https://cdn.cloudflare.steamstatic.com/steam/apps/292030/header.jpg?t=1668443314",
+            "picutres":[
+                "https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.webm?t=1528288687" //trailer
+            ],
+            "liked": false,
+            "disliked": false,
+            "inCart": false,
+            "tags": ["Open World", "RPG", "Story Rich", "Atmospheric"],
+        },
+        {
+            "id": 14,
+            "name": "TEKKEN 7",
+            "description": "Discover the epic conclusion of the long-time clan warfare between members of the Mishima family. Powered by Unreal Engine 4, the legendary fighting game franchise fights back with stunning story-driven cinematic battles and intense duels that can be enjoyed with friends and rivals.",
+            "price": 49.99,
+            "displayPicture": "https://cdn.cloudflare.steamstatic.com/steam/apps/389730/header.jpg?t=1666949098",
+            "picutres":[
+                "https://cdn.cloudflare.steamstatic.com/steam/apps/256864020/movie480_vp9.webm?t=1638994619" //trailer
+            ],
+            "liked": false,
+            "disliked": false,
+            "inCart": false,
+            "tags": ["Fighting", "Action", "Multiplayer", "Competitive"],
+        },
+        {
+            "id": 15,
+            "name": "SOULCALIBUR VI",
+            "description": "Bring more than your fists to the fight! Featuring all-new battle mechanics and characters, SOULCALIBUR VI marks a new era of the historic franchise. Welcome back to the stage of history!",
+            "price": 79.99,
+            "displayPicture": "https://cdn.cloudflare.steamstatic.com/steam/apps/544750/header.jpg?t=1646956219",
+            "picutres":[
+                "https://cdn.cloudflare.steamstatic.com/steam/apps/256807455/movie480_vp9.webm?t=1604033038" //trailer
+            ],
+            "liked": false,
+            "disliked": false,
+            "inCart": false,
+            "tags": ["Fighting", "Action", "Character Customization", "Arcade"],
+        },
+        {
+            "id": 16,
+            "name": "Mortal Kombat 11",
+            "description": "Mortal Kombat is back and better than ever in the next evolution of the iconic franchise.",
+            "price": 69.99,
+            "displayPicture": "https://cdn.cloudflare.steamstatic.com/steam/apps/976310/header.jpg?t=1654216426",
+            "picutres":[
+                "https://cdn.cloudflare.steamstatic.com/steam/apps/256813084/movie480_vp9.webm?t=1607567246" //trailer
+            ],
+            "liked": false,
+            "disliked": false,
+            "inCart": false,
+            "tags": ["Fighting", "Gore", "Violent", "Multiplayer", "Blood"],
         }
     ]
 }

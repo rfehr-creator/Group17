d43040b4f2358b1268eb31d24a51d8bf49c56e0c
rfehr-creator <62805035+rfehr-creator@users.noreply.github.com>
2022-11-24 21:42:29 -0600

    LikedList

--- likedList.js
+++ likedList.js
@@ -6,34 +6,23 @@ function DisplayLikedList(back) {
 
     var cartItems = getLikedGames();
 
-    // clear search results table
-    clearTable("LikedResultTable");
-    var table = document.getElementById('LikedResultTable');
-
-    // create header row
-    var headRow = table.insertRow(0);
-    headRow.insertCell(0).outerHTML = "<th>Name</th>";
-    headRow.insertCell(1).outerHTML = "<th>Description</th>";
-    headRow.insertCell(2).outerHTML = "<th>Price</th>";
-
 
     if (cartItems.length > 0) {
         sortResults(cartItems, "LikedSort");
     }
 
+
+    var container = document.getElementById('likedListItemsContainer')
+    container.innerHTML = "";
+
     for (let index = 0; index < cartItems.length; index++) {
         var game = cartItems[index];
-
-        var row = table.insertRow(table.rows.length);
-
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-
-        cell1.innerHTML = game.name;
-        cell2.innerHTML = game.description;
-        cell3.innerHTML = game.price;
+        addGameLikedList(game,container)
     }
+}
+
+function addGameLikedList(game,element){
+    element.innerHTML = element.innerHTML +""
 }
 
 function DisplayDislikedList(back) {

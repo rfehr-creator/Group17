1fbc7d7ad80c8b90da809556a88dc519fc518515
rfehr-creator <62805035+rfehr-creator@users.noreply.github.com>
2022-11-27 10:45:31 -0600

    Updated liked and disliked functions

--- game.js
+++ game.js
@@ -32,6 +32,7 @@ function addToLikedlist(id){
         const game = games[index];
         if (game.id === id) {
             game.liked = true;
+            game.disliked = false;
         }
     }
     storeGames(games);

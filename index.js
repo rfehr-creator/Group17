dcccf55b0e25d37a6e3a2c3f965b506b9bdff882
rfehr-creator <62805035+rfehr-creator@users.noreply.github.com>
2022-11-27 13:13:19 -0600

    Changed JS files

--- index.js
+++ index.js
@@ -261,3 +261,196 @@ function popHistory() {
     localStorage.setItem('history', JSON.stringify(history));
     updateBackButton();
 }
+
+function searchGames(back) {
+    var keyword = document.getElementById('searchBox').value.toLowerCase();
+    if (keyword != null && keyword != "") {
+        if (back == null || back == false) addHistory("searchPage", keyword);
+        hideElements();
+        document.getElementById('searchResults').style.display = "block";
+        var title = document.getElementById('searchResultsTitle');
+        title.innerHTML = "Search Results for " + "'"+ keyword +"'"
+
+        var games = loadGames();
+        var searchResults = [];
+        for (let index = 0; index < games.length; index++) {
+            var game = games[index];
+
+            // add game if name, description or price match
+            if (game.name.toLowerCase().includes(keyword) || game.description.toLowerCase().includes(keyword) || game.price == keyword) {
+                searchResults.push(game);
+            } else {
+
+
+                // add game if tags match
+                for (let tagIndex = 0; tagIndex < game.tags.length; tagIndex++) {
+                    const tag = game.tags[tagIndex];
+
+                    if (tag.toLowerCase().includes(keyword)) {
+                        searchResults.push(game);
+                    }
+                }
+            }
+        }
+
+        if (searchResults.length > 0) {
+            searchResults = sortResults(searchResults, "searchSort");
+        }
+
+        var container = document.getElementById('SearchResultContainerItems')
+        container.innerHTML = "";
+
+        // add search results to table
+        for (let index = 0; index < searchResults.length; index++) {
+            const game = searchResults[index];
+
+            addSearchResultGame(game, container);
+        }
+    }
+}
+
+function searchGameTag(keyword, back) {
+    if (keyword != null && keyword != "") {
+        if (back == null || back == false) addHistory("searchPage", keyword);
+        hideElements();
+        document.getElementById('searchResults').style.display = "block";
+
+        var games = loadGames();
+        var searchResults = [];
+        
+        for (let index = 0; index < games.length; index++) {
+            var game = games[index];
+
+            // add game if tags match
+            for (let tagIndex = 0; tagIndex < game.tags.length; tagIndex++) {
+                const tag = game.tags[tagIndex];
+
+                if (tag.toLowerCase().includes(keyword.toLowerCase())) {
+                    searchResults.push(game);
+                }
+            }
+        }
+        
+        if (searchResults.length > 0) {
+            searchResults = sortResults(searchResults, "searchSort");
+        }
+
+        var container = document.getElementById('SearchResultContainerItems')
+        container.innerHTML = "";
+
+        // add search results to table
+        for (let index = 0; index < searchResults.length; index++) {
+            const game = searchResults[index];
+
+            addSearchResultGame(game, container);
+        }
+    }
+}
+
+function addSearchResultGame(game, element) {
+    var price = "$"+game.price;
+    if (game.price <= 0) {
+        price = "Free";
+    }
+    var tags = "";
+    game.tags.forEach(tag => {
+        tags = tags + '<text class="gameDetailsTagText" onclick="searchGameTag(\'' + tag + '\')">' + tag + '</text>'
+    });
+    element.innerHTML = element.innerHTML +
+        '  <div class="displayItem">' +
+        '  <table class="displayItemTable">' +
+        '    <tr>' +
+        '      <td class="displayItemImgCol">' +
+        '        <img class="displayItemImg"' +
+        '          src="' + game.displayPicture + '">' +
+        '      </td>' +
+        '      <td class="displayItemDescCol">' +
+        '        <p class="displayItemDescTitle">' + game.name +
+        '          <br><br>' +
+                    tags +
+        '          <text class="displayItemDescText">' + game.description + '</text>' +
+        '        </p>' +
+        '        <p class="displayItemDescTitle">' + price + '</p>' +
+        '      </td>' +
+        '      <td class="displayItemButtonCol">' +
+        '       <button class="displayItemButton" onclick="addToLikedlist('+ game.id +')">Add to Liked</button>' +
+        '       <br>' +
+        '       <br>' +
+        '       <button class="displayItemButton" onclick="addToCart('+game.id+')">Add to Cart</button>' +
+        '      </td>' +
+        '    </tr>' +
+        '  </table>' +
+        '</div>'
+}
+
+// elements that should be displayed and hidden for home screen
+function home() {
+    hideElements();
+    document.getElementById('mainDisplayTable').style.display = "table";
+    document.getElementById('gameArea').style.display = "block";
+
+    // Show main table when details page is closed
+    document.getElementById('mainDisplayTable').style.visibility = "visible";
+    displayRandomGame();
+    addLikedGamesToGameDiv();
+    updateBackButton();
+}
+
+function discoveryButton(){
+    home();
+    localStorage.setItem('history', null);
+    addHistory("home", "");
+    updateBackButton();
+}
+
+// update back button
+function updateBackButton(){
+    var len = getHistory().length;
+
+    var backButton = document.getElementById("backButton");
+    if(len <= 1){
+        backButton.style = "opacity: 0.4";
+    }
+    else{
+        backButton.style = "opacity: 1";
+    }
+}
+
+// display liked games in gameDiv
+function updateHeart() {
+    var heartImg = document.getElementById('heartPathTopbar');
+    if (getLikedGames().length > 0) {
+        // heartImg.src = "https://cdn-icons-png.flaticon.com/128/833/833472.png";
+        heartImg.style = "fill: red;";
+    }
+    else {
+        // heartImg.src = "https://img.icons8.com/neon/512/experimental-hearts-neon.png";
+        heartImg.style = "fill: #282828; stroke: red; stroke-width: 15px";
+    }
+}
+
+
+function sortResults(games, element) {
+    var sort = document.getElementById(element);
+
+    if (sort.options[sort.selectedIndex].value === "sortLowestPrice") {
+        games.sort(function (a, b) {
+            return a.price - b.price;
+        });
+    } else if (sort.options[sort.selectedIndex].value === "sortHighestPrice") {
+        games.sort(function (a, b) {
+            return b.price - a.price;
+        });
+    } else {
+        games.sort(function (a, b) {
+            if(a.name.toLowerCase() < b.name.toLowerCase()){
+                return -1;
+            }
+            if(a.name.toLowerCase() > b.name.toLowerCase()){
+                return 1;
+            }
+            return 0;
+        });
+    }
+    return games;
+}
